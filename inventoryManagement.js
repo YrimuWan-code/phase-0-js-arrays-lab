d36bb2cba10ce5c00f12322feec2c115aa18f17b
Yrimu-Wan-code <patiencegachie@gmail.com>
2026-04-08 14:22:28 +0300

    Complete arrays Lab

--- inventoryManagement.js
+++ inventoryManagement.js
@@ -1,5 +1,29 @@
 // Write your code here
+products =["Laptop", "Phone", "Headphones", "Monitor"];
+// Function for the first product details
+// let first= products[0];
+function logFirstProduct() {
+    console.log(products [0]);
+}
 
+// Function for adding a product
+function addProduct() {
+  products.push("Tablet");
+}
+addProduct(products);
+
+//Function for updating a product name
+function updateProductName(newName) {
+  products[1] = "Smartphone"
+}
+updateProductName(products[1]);
+console.log(products);
+
+//Function to remove last product from array
+function removeLastProduct() {
+products.pop();
+}
+console.log(products);
 
 
 // Export the necessary parts for testing
